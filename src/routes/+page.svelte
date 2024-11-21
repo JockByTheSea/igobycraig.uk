7f7491414b890fb91a8c3a4697f3ba4548d4b6fd
Craig Thompson <craig@thompsonyorks.co.uk>
2024-11-21 01:10:45 +0000

    added extra para and bluesky logo

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,9 @@
 <script>
 	import github from '$lib/assets/img/github-mark-white.svg';
 	import linkedIn from '$lib/assets/img/In-White-48.png';
+	import bluesky from '$lib/assets/img/Bluesky_Logo.svg';
 </script>
 
-<!-- <container
-	class="flex min-h-screen w-screen items-center justify-center bg-gradient-to-b from-zinc-800 to-zinc-950 p-4"
-> -->
 <main class=" w-full max-w-screen-xl p-8 text-left">
 	<section class="mb-10">
 		<h1 class="font-hostgrotesk mb-5 text-6xl font-bold leading-snug text-white">
@@ -16,9 +14,12 @@
 			I'm a technical leader that turns teams into great teams, developers into happy developers and
 			product ideas into reality.
 		</p>
-		<p class="max-w-2xl text-xl leading-relaxed text-zinc-200">
+		<p class="mb-5 max-w-2xl text-xl leading-relaxed text-zinc-200">
 			I've worked on everything; from frontend to backend, monoliths, distributed, event-driven,
 			embedded IoT, platform and infrastructure
+		</p>
+		<p class="max-w-2xl text-xl leading-relaxed text-zinc-200">
+			Come and say hello! You can hit me up on one of these networks
 		</p>
 	</section>
 
@@ -27,6 +28,8 @@
 		<a href="https://linkedin.com/in/craig-thompson-js"
 			><img src={linkedIn} alt="LinkedIn" class="size-10" /></a
 		>
+		<a href="https://bsky.app/profile/igobycraig.uk"
+			><img src={bluesky} alt="Bluesky" class="size-10" /></a
+		>
 	</section>
 </main>
-<!-- </container> -->
